f32af984db47f9c1c6dbb13dce2e1cec04e802a2
Jingwei Yang <18220361785@163.com>
2026-08-18 19:07:45 +0800

    Create inference_reference_guided.py

---
+++ inference_reference_guided.py
@@ -0,0 +1,78 @@
+import torch
+from PIL import Image
+from diffsynth.pipelines.qwen_image import QwenImagePipeline, ModelConfig
+import os
+
+pipe = QwenImagePipeline.from_pretrained(
+    torch_dtype=torch.bfloat16,
+    device="cuda",
+    model_configs=[
+        ModelConfig(
+            model_id="Qwen/Qwen-Image-Edit-2509",
+            download_source='huggingface',
+            origin_file_pattern="transformer/diffusion_pytorch_model*.safetensors"
+        ),
+        ModelConfig(
+            model_id="Qwen/Qwen-Image-Edit-2509",
+            download_source='huggingface',
+            origin_file_pattern="text_encoder/model*.safetensors"
+        ),
+        ModelConfig(
+            model_id="Qwen/Qwen-Image-Edit-2509",
+            download_source='huggingface',
+            origin_file_pattern="vae/diffusion_pytorch_model.safetensors"
+        ),
+    ],
+    tokenizer_config=None,
+    processor_config=ModelConfig(
+        model_id="Qwen/Qwen-Image-Edit-2509",
+        download_source='huggingface',
+        origin_file_pattern="processor/"
+    ),
+)
+
+# load lora
+qwen_image_style_transfer_lora_model = './Qwen-Image-Edit-2509-unicsg.safetensors'
+qwen_image_speedup_lora_model = './Qwen-Image-Edit-2509-Lightning-4steps-V1.0-bf16.safetensors'
+pipe.load_lora(pipe.dit, qwen_image_style_transfer_lora_model)
+pipe.load_lora(pipe.dit, qwen_image_speedup_lora_model)
+
+content_ref = "./test_content.jpg"
+style_ref = "./test_style.jpg"
+prompt = 'Transform the image into 3D Chibi style. The style of the image is the same as in Image 1.'
+
+# content image resize
+w, h = Image.open(content_ref).convert("RGB").size
+minedge = 1024
+if w > h:
+    r = w / h
+    h = minedge
+    w = int(h * r) - int(h * r) % 16
+else:
+    r = h / w
+    w = minedge
+    h = int(w * r) - int(w * r) % 16
+
+images = [
+    Image.open(content_ref).convert("RGB").resize((w, h)),
+    Image.open(style_ref).convert("RGB").resize((minedge, minedge))
+]
+
+# reference‑guided edit_image=[content, style]
+image = pipe(
+    prompt,
+    edit_image=images,
+    seed=123,
+    num_inference_steps=4,
+    height=h,
+    width=w,
+    edit_image_auto_resize=False,
+    cfg_scale=1.0
+)
+
+save_dir = './output/'
+os.makedirs(save_dir, exist_ok=True)
+prefix = style_ref.split('/')[-1].split('.')[0]
+out_path = os.path.join(save_dir, f'{prefix}_reference_result.png')
+image.save(out_path)
+print(f"saved to {out_path}")
